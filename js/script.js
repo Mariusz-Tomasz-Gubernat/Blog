131a838144aae29ff425957ae4e57512af98b53a
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2024-07-17 10:11:54 +0200

    add functions of remove class active of links and articles

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+const activeArticles = document.querySelectorAll('.posts .post.active')
+
+function titleClickHandler(event) {
+  console.log('Link was clicked!');
+  console.log(event);
+
+  /* remove class 'active' from all article links  */
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active')
+  }
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
